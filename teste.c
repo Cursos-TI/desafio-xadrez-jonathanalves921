2caea2985f0d26c2a27f7147f48ee72bc107be6e
jonathanalves921 <jonathanalvess.bd@gmail.com>
2025-02-20 19:35:53 +0000

    xadrez dificuldade aventureiro

---
+++ teste.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+
+
+for (int c = 0; c < 2; c++){
+    printf("Cavalo:");
+    printf("Baixo/n");
+
+    int c2 = 0;
+    while(c2 < 1){
+        printf("Cavalo:");
+        printf("Direita\n");
+        c2++;
+    }
+}
--- xadrez.c
+++ xadrez.c
@@ -6,21 +6,34 @@ for (int i = 0; i < 5; i++){
     printf("Torre:");
     printf("Direita!\n");//imprime o valor de 5 casas para direita
 }
-int b; //variavel do bispo
+printf("\n");
+
+int b = 0; //variavel do bispo
 
 while(b < 5){ //faz o bispo andar 5 casas
     printf("Bispo:");
     printf("Direita, Cima!\n");//imprime o valor de cinco casas (direita diagonal)
     b++;
 }
-int r;//variavel da rainha
+printf("\n");
+
+int r = 0;//variavel da rainha
 do{
     printf("Rainha:");
     printf("Esquerda\n"); //imprime o valor de 8 casas para a direita
     r++;
 
 }while (r < 8);
+printf("\n");
 
+//movimentação do cavalo
+for (int l1 = 0; l1 < 1; l1++) {
+    for (int l2 = 0; l2 < 2; l2++) {
+        printf("Cavalo: Baixo\n");
+    }
+    printf("Cavalo: Direita\n");
+}
+printf("\n");
     return 0;
 }
 
